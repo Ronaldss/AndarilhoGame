7b832e8e5571e77213c37998bf35ed9906d4a48e
RonaldsS <ronald_ss@outlook.com>
2019-09-29 20:57:58 -0300

    Upload de arquivos corretos

---
+++ ambiente.js
@@ -0,0 +1,33 @@
+/* Estudando estruturas de LAÇO, REPETIÇÕES ou INTERAÇÕES.
+
+USANDO A FORÇA BRUTA
+
+console.log("Olá tudo bem!")
+console.log("Olá tudo bem!")
+console.log("Olá tudo bem!")
+console.log("Olá tudo bem!")
+console.log("Olá tudo bem!")
+console.log("Olá tudo bem!")
+*/
+/*
+
+TESTA PRIMEIRO E DEPOIS EXECUTA O BLOCO
+
+contador = 1
+while(contador < 7){
+    console.log("Olá tudo bem!")
+    contador++
+}
+*/
+
+/*
+
+EXECUTA O BLOCO E DEPOIS TESTA
+
+contador = 1
+do {
+    console.log(`${contador} - Olá, tudo bem!`)
+    contador++
+}while(contador <= 10)
+
+*/
